ea80972cc07612f0d95a86b90e7bef7d7f8f8900
Common Lips User <mikael@gnutiken.se>
2012-05-09 10:15:54 +0200

    Updated software packages in quicklisp with: (ql:update-all-dists).

--- dists/quicklisp/software/weblocks-20120305-git/examples/weblocks-demo/rundemo.lisp
+++ dists/quicklisp/software/weblocks-20120305-git/examples/weblocks-demo/rundemo.lisp
@@ -1,5 +1,4 @@
-(push #p"/home/cl-user/quicklisp/dists/quicklisp/software/weblocks-20120305-git/" asdf:*central-registry*)
-(push #p"/home/cl-user/quicklisp/dists/quicklisp/software/weblocks-20120305-git/examples/weblocks-demo/" asdf:*central-registry*)
+(push #p"/usr/lib/sbcl/site/cl-weblocks/" asdf:*central-registry*)
+(push #p"/usr/lib/sbcl/site/cl-weblocks/examples/weblocks-demo/" asdf:*central-registry*)
 (asdf:operate 'asdf:load-op :weblocks-demo)
 (weblocks-demo:start-weblocks-demo)
-
